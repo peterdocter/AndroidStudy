bb0a6432eec86ce890b071937cba2adb0749f8dd
Gavin <chentong_think@126.com>
2014-08-13 21:56:56 +0800

    ndk  c语言

---
+++ NDK/1.C/HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>   // import java.lang;  函数库的声明。  standard input output 
+#include <stdlib.h>  // standard library
+main()
+{
+      printf("Hello world !\n"); //sysout
+      system("java HelloWorld"); //C代码调用java 
+      system("pause");//相当于cmd pause  ，让界面暂停，方便观察结果显示 
+}
