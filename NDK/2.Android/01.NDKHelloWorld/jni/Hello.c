472c69066bb030678afc5907d1e12e034d3ec9a1
wizchentong <chentong@wiz.cn>
2014-10-05 17:13:38 +0800

    javah版改进

--- NDK/2.Android/01.NDKHelloWorld/jni/Hello.c
+++ NDK/2.Android/01.NDKHelloWorld/jni/Hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <jni.h>
+#include "com_czt_ndk_helloworld_MainActivity.h"
 //2.实现Java里面定义的c方法。
 //函数的声明包括两个部分：1.函数名称2.函数的返回值和接受的参数
 jstring Java_com_czt_ndk_helloworld_MainActivity_helloFromC(JNIEnv* env,jobject obj){
@@ -18,3 +19,7 @@ jstring Java_com_czt_ndk_helloworld_MainActivity_hello_1from_1c(JNIEnv* env,jobj
 }
 //3.c代码写好后，需要把c代码编译成一个可执行的二进制文件。--->.so
 //4.编写android.mk文件把c代码编译成一个so文件。
+JNIEXPORT jstring JNICALL Java_com_czt_ndk_helloworld_MainActivity_helloFromJavahC
+  (JNIEnv * env, jobject obj){
+	return (*env)->NewStringUTF(env, "hello from javah c!");
+}
